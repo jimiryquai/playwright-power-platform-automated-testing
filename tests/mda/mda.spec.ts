6e787505e31b452f4a19f3fbec23975b5ea04122
Ryan, James <james.ryan@capgemini.com>
2025-09-12 16:51:36 +0000

    Updated mda.spec.ts

--- tests/mda/mda.spec.ts
+++ tests/mda/mda.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '@pages/LoginPage';
-import { XrmHelper } from '@utils/XrmHelper';
+import { LoginPage } from '../../pages/LoginPage';
+import { XrmHelper } from '../../utils/XrmHelper';
 import 'dotenv/config';
 
  
