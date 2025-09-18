40dd0dc3cc6b253dc4b3c9ed81cff8b21f032de4
Constantin.Dimitrenco <Constantin.Dimitrenco@traderemedies.gov.uk>
2025-09-18 15:31:03 +0100

    remade TCs 4177 4178 (4777 fails as expected, bug raised with devs)

---
+++ tests/public-file/UpdatePublicFilecase.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import { testConfig } from './TestConfig';
+test.use({
+  storageState: 'auth/public-file.json'
+});
+
+test('4177', async ({ page }) => {
+  await page.goto(testConfig.azureAppUrl);
+  await page.getByRole('row', { name: 'No. 1147/Draft Case New Draft' }).getByRole('link').nth(2).click();
+
+  await expect(page.getByRole('link', { name: 'Read the guidance documents (' })).toHaveAttribute('href', 'https://www.gov.uk/government/publications/the-uk-trade-remedies-investigations-process/the-tras-investigation-process)');
+  await page.getByRole('link', { name: '/Draft@traderemedies.gov.uk' }).click();
+
+});
+
+test('4178', async ({ page }) => {
+  await page.goto(testConfig.azureAppUrl);
+  await page.getByRole('row', { name: 'No. 1147/Draft Case New Draft' }).getByRole('link').nth(2).click();
+
+  await expect(page.getByRole('link', { name: '/Draft@traderemedies.gov.uk' })).toHaveAttribute('href', 'mailto:1147/Draft@traderemedies.gov.uk');
+
+});
