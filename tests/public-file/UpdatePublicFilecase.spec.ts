339df52fef7f18c7b427c70fa849a557444ecd53
Constantin.Dimitrenco <Constantin.Dimitrenco@traderemedies.gov.uk>
2025-09-19 15:56:25 +0100

    TC 4168

--- tests/public-file/UpdatePublicFilecase.spec.ts
+++ tests/public-file/UpdatePublicFilecase.spec.ts
@@ -20,3 +20,12 @@ test('4178', async ({ page }) => {
   await expect(page.getByRole('link', { name: '/Draft@traderemedies.gov.uk' })).toHaveAttribute('href', 'mailto:1147/Draft@traderemedies.gov.uk');
 
 });
+
+test('4168', async ({ page }) => {
+  await page.goto(testConfig.azureAppUrl);
+  await page.getByRole('row', { name: 'No. 1147/Draft Case New Draft' }).getByRole('link').nth(2).click();
+
+  await expect(page.getByRole('link', { name: 'Read the guidance documents (' })).toHaveAttribute('href', 'https://www.gov.uk/trade-tariff');
+  await page.getByRole('link', { name: '/Draft@traderemedies.gov.uk' }).click();
+
+});
