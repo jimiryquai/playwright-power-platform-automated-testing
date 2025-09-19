ba94a67fa6fef824a2b8d1c211e73ddf5fdbe766
Constantin.Dimitrenco <Constantin.Dimitrenco@traderemedies.gov.uk>
2025-09-19 14:08:26 +0100

    Regoster Interest screen link validation test

---
+++ tests/public-file/RegisterInterestLinkScreen.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test.use({
+  storageState: 'auth/public-file.json'
+});
+
+test('4166', async ({ page }) => {
+  await page.goto('https://publicfile-test.tangoromeoalpha.co.uk/');
+  await page.getByRole('row', { name: 'No. 1147/Draft Case New Draft' }).getByRole('link').nth(2).click();
+  await page.getByLabel('Breadcrumb').getByText('Start a new registration of').click();
+  await page.getByText('The name of the organisation').click();
+  await page.locator('#main-content').click();
+  await expect(page.getByRole('button', { name: 'Start registration of interest' })).toBeVisible();
+});
