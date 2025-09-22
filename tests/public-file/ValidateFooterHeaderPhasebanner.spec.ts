0341ea49ddcb0d3c41e84bc45d390839d2f165e7
Constantin.Dimitrenco <Constantin.Dimitrenco@traderemedies.gov.uk>
2025-09-22 13:46:20 +0100

    added TC 4162

--- tests/public-file/ValidateFooterHeaderPhasebanner.spec.ts
+++ tests/public-file/ValidateFooterHeaderPhasebanner.spec.ts
@@ -32,7 +32,7 @@ test('4163', async ({ page }) => {
   await page.getByText('Skip to main content GOV.UK').press('ControlOrMeta+-');
   await page.getByText('Skip to main content GOV.UK').press('ControlOrMeta+-');
   await page.getByText('Skip to main content GOV.UK').press('ControlOrMeta+-');
-  
+
   await expect(page.locator('body')).toMatchAriaSnapshot(`
     - paragraph:
       - strong: beta
@@ -41,7 +41,7 @@ test('4163', async ({ page }) => {
         - /url: ""
       - text: will help us to improve it.
     `);
-    await expect(page.getByRole('contentinfo')).toMatchAriaSnapshot(`
+  await expect(page.getByRole('contentinfo')).toMatchAriaSnapshot(`
     - contentinfo:
       - heading "Support links" [level=2]
       - list:
@@ -63,11 +63,46 @@ test('4163', async ({ page }) => {
     `);
   await expect(page.getByText('This is a new service – your')).toBeVisible()
 
-await expect(page.getByRole('link', { name: 'Sign in', exact: true }))
-  .toHaveAttribute('href', testConfig.portalUrl);
-await expect(page.getByRole('link', { name: 'TRA Investigations' }))
-  .toHaveAttribute('href', testConfig.azureAppUrl);
+  await expect(page.getByRole('link', { name: 'Sign in', exact: true }))
+    .toHaveAttribute('href', testConfig.portalUrl);
+  await expect(page.getByRole('link', { name: 'TRA Investigations' }))
+    .toHaveAttribute('href', testConfig.azureAppUrl);
 
-await expect(  page.getByLabel('Menu').getByRole('link', { name: 'Home' }))
-  .toHaveAttribute('href', testConfig.azureAppUrl);
+  await expect(page.getByLabel('Menu').getByRole('link', { name: 'Home' }))
+    .toHaveAttribute('href', testConfig.azureAppUrl);
+});
+test('4162', async ({ page }) => {
+  await page.goto(testConfig.azureAppUrl);
+  await page.getByRole('link', { name: 'Cookies' }).click();
+  await expect(page.locator('#main-content')).toMatchAriaSnapshot(`
+    - heading "Cookies on the Trade Remedies Digital service" [level=1]
+    - paragraph: Cookies are files saved on your phone, tablet or computer when you visit a website.
+    - paragraph: We use cookies to store information about how you use the Trade Remedies website, such as the pages you visit.
+    - heading "Cookie settings" [level=3]
+    - paragraph: We use two types of cookie. You can choose which cookies you’re happy for us to use.
+    - heading "Cookies that measure website use" [level=4]
+    - paragraph: "We use Google Analytics to measure how you use the website so we can improve it based on user needs. Google Analytics sets cookies that store anonymised information about:"
+    - list:
+      - listitem: how you got to the site
+      - listitem: the pages you visit on GOV.UK and government digital services, and how long you spend on each page
+      - listitem: what you click on while you’re visiting the site
+    - paragraph: We do not allow Google to use or share the data about how you use this site.
+    - group:
+      - radio "On"
+      - text: "On"
+      - radio "Off"
+      - text: "Off"
+    - heading "Strictly necessary cookies" [level=4]
+    - paragraph: These essential cookies do things like remember your progress through a form (for example a licence application)
+    - paragraph: They always need to be on.
+    - paragraph:
+      - link "Find out more about cookies on the TRA Digital Service":
+        - /url: https://www.trade-remedies.service.gov.uk/cookiepolicy/
+    - button "Save changes"
+    `);
+  await page.getByRole('group').locator('div').nth(1).click();
+  await page.getByRole('group').locator('div').nth(2).click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('link', { name: 'Find out more about cookies' }).click();
+  const page1 = await page1Promise;
 });
